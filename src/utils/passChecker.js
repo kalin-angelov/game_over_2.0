14733a674322648e2982c114b53f5dca6aa1a348
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-12-07 11:56:04 +0200

    edit: add regex for the pass to check if it meets the requirements

--- src/utils/passChecker.js
+++ src/utils/passChecker.js
@@ -4,6 +4,10 @@ export const passChecker = (pass, rePass) => {
         throw new Error("All fields are required.");
     }
 
+    if (/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/gm.test(pass)) {
+        throw new Error("Invalid password.")
+    }
+//Password must be at least 8 chars long and must contain at least one Capital letter, one Special symbol and one digit.
     if (pass !== rePass) {
         throw new Error("Passwords don't match.")
     }
