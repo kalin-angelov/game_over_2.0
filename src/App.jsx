38da6a0a4615a31187aa55de24d973a52cce9288
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-12-14 23:34:21 +0200

    edit: add path to create component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/Home/Home"
 import GameList from "./components/Posts/GameList/GameList";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import Create from "./components/Create/Create";
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/catalog" element={<GameList />} />
+                <Route path="/create" element={<Create />} />
             </Routes>
         </>
     );
